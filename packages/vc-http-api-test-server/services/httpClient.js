a353cc4ea88753fa6da769e367f8f90740e549ed
gjgd <gjgd@protonmail.com>
2021-06-17 16:36:00 -0500

    Revert "chore: add abort logic for the tests"
    This reverts commit b966caacd282af057e0eaf5474b6f418dd050696.

--- packages/vc-http-api-test-server/services/httpClient.js
+++ packages/vc-http-api-test-server/services/httpClient.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const AbortController = require('abort-controller');
 
 const getJson = async (url, requestAuthorization) => {
   let headers = {
@@ -55,27 +54,14 @@ const postJson = async (url, body, requestAuthorization = {}) => {
     urlWithParams = `${url}?${params.toString()}`;
   }
 
-  // Abort request if it takes more than 1s
-  const controller = new AbortController();
-  const timeout = setTimeout(() => {
-    controller.abort();
-  }, 3000);
-  try {
-    const res = await fetch(urlWithParams, {
-      headers,
-      method: 'post',
-      body: JSON.stringify(body),
-      signal: controller.signal,
-    });
-    const resBody = await res.json();
-    return { status: res.status, body: resBody };
-  } catch (error) {
-    if (error && error.type === 'aborted') {
-      return { status: 408 };
-    }
-  } finally {
-    clearTimeout(timeout);
-  }
+  const res = await fetch(urlWithParams, {
+    headers,
+    method: 'post',
+    body: JSON.stringify(body),
+  });
+  const resBody = await res.json();
+
+  return {status: res.status, body: resBody};
 };
 
 module.exports = {
